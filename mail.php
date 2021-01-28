eba4cc86ce140316360868ef1e4def5af2d73c67
Maria <60785083+sereginamaria@users.noreply.github.com>
2021-01-28 09:24:08 +0300

    'Init'

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$to = 'v44882266@mail.ru'; // адрес получателя
+$subject = 'Заказ обратного звонка'; // тема письма
+$message = "Имя: {$_POST['name']}\r\n"; // добавляем имя в текст
+$message .= "Телефон: {$_POST['phone']}"; // добавляем телефон в текст
+$headers .= 'Content-type: text/plain; charset=utf-8' . "\r\n"; // установливаем кодировку
+$headers .= 'From: mail@site.com' . "\r\n"; // добавляем отправителя
+
+if( mail($to, $subject, $message, $headers) ){
+	echo '<p style="color: green;">Ваше сообщение отправлено</p>';
+}else{
+	echo '<p style="color: red;">Ошибка!</p>';
+}
